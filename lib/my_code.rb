b0165d33fb408d96a339b98568ff3364d7d3dcec
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 20:30:19 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # Your Code Here
+
+method_using_block { puts "hi" }
+ 
+method_using_block do
+  puts "hi"
+end
+
+
+def make_sandwich(element1, element2)
+end
+ 
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2}"
+  puts base
+  yield
+  base
+end
+ 
+
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2}"
+  yield(base)
+end
+ 
+
+def make_sandwich(element1, element2)
+  yield("A #{element1} and #{element2} sandwich")
+end
+ 
+
+
+
