3f642cb7f0f77f948c51b2e4ad065a4d47afcd95
Sean Eriksen <eriksensean@yahoo.com>
2019-10-23 20:26:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,3 +11,14 @@ def make_sandwich(element1, element2)
 end
  
 make_sandwich("Peanut Butter", "Yakisoba")
+
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2}"
+  puts base
+  yield
+  base
+end
+ 
+make_sandwich("chicken", "a sense of malaise") do |innards|
+  puts "making some tasty stuff..."
+end
